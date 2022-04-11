f403c238dfe9487aafe07111652a97f96186448a
shaikhsirin <shaikhsirin8080example.com>
2022-04-11 12:51:33 +0530

    Added:-Adding New Contact in Address Book

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,39 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class AddressBook {
-    private class Contact {
-        String firstName, lastNmae, address, city, state, emailId;
-        int zipCode;
-        long mobileNumber;
+    static ContactDetails person = new ContactDetails();
+    /**
+     * Creating a List of ContactDetails of type String using ArrayList here Adding
+     * new elements in the List
+     */
+    static List<ContactDetails> contactDetailsList =  new ArrayList<ContactDetails>();
+
+    /**
+     * created method addNewContact() to create a new contacts to the AddressBook
+     */
+    public static void addNewContact() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Name : ");
+        String firstName = scanner.next();
+        System.out.println("Enter Last Name : ");
+        String lastName = scanner.next();
+        System.out.println("Enter Your Address : ");
+        String address = scanner.next();
+        System.out.println("Enter Your City : ");
+        String city = scanner.next();
+        System.out.println("Enter Your State");
+        String state = scanner.next();
+        System.out.println("Enter ZipCode : ");
+        int zipCode = scanner.nextInt();
+        System.out.println("Enter MobileNumber : ");
+        long mobileNumber = scanner.nextLong();
+        System.out.println("Enter your EmailId  : ");
+        String emailId = scanner.next();
+        scanner.close();
+
+        person = new ContactDetails(firstName, lastName, address, city, state, zipCode, mobileNumber, emailId);
+        contactDetailsList.add(person);
+        printContact();
+
     }
 
     /**
-     * created method printContact for creating contacts in AddressBook
+     * created method printContact() to display the data
      */
-    public void printContact() {
-        Contact person = new Contact();
-        person.firstName = "Sirin";
-        person.lastNmae = "Shaikh";
-        person.address = "kolhapur";
-        person.city = "Icgalkaranji";
-        person.state = "Maharashtra";
-        person.zipCode = 416115;
-        person.mobileNumber = 998875550;
-        person.emailId = "sirinshaikh942@gmail.com";
-        System.out.println("Contact Details");
-        System.out.println("Name          : " + person.firstName + " " + person.lastNmae + "\n" + "Address       : "
-                + person.address + "\n" + "City          : " + person.city + "\n" + "State         : " + person.state
-                + "\n" + "ZipCode       : " + person.zipCode + "\n" + "MobileNumber  : " + person.mobileNumber + "\n"
-                + "EmailId       : " + person.emailId + "\n");
+    public static void printContact() {
+        for (int i = 0; i < contactDetailsList.size(); i++) {
+            person = contactDetailsList.get(i);
+            System.out.println("Contact Details");
+            System.out.println("Name          : " + person.getFirstName() + " " + person.getLastName() + "\n"
+                    + "Address       : " + person.getAddress() + "\n" + "City          : " + person.getCity() + "\n"
+                    + "State         : " + person.getState() + "\n" + "ZipCode       : " + person.getZipCode() + "\n"
+                    + "MobileNumber  : " + person.getMobileNumber() + "\n" + "EmailId       : " + person.getEmailId()
+                    + "\n");
+        }
     }
 
     public static void main(String[] args) {
         /**
-         * created instance of AddressBook class
+         * calling method addNewContact()
          */
-        AddressBook addressBook = new AddressBook();
-        /**
-         * Calling method printContact for displaying contacts
-         */
-        addressBook.printContact();
-
+        addNewContact();
     }
 }
